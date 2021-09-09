e96d5b4bbf4271a468f6d03fd0c75d0b207b7c61
iamchandrakanth <ccmchandrakanth5@gmail.com>
2021-09-09 08:06:27 +0530

    base starter

---
+++ index.js
@@ -0,0 +1,24 @@
+// Assign variables using query selectors
+
+
+
+
+// initialise days and months array
+
+
+
+
+// toggle dark mode and light mode using click handlers
+
+
+
+
+// setTime() function
+
+
+
+// scale property
+
+
+
+// run
